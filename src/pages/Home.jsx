17dc1d8883cc0f974fa4478d1be6addc10199ab6
Praveshkacode <pravesh182003@gmail.com>
2025-04-22 23:08:02 +0530

    Create Context and Banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import TopExperts from '../components/TopExperts'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Header/>
       <SpecialityMenu/>
       <TopExperts/>
+      <Banner/>
     </div>
   )
 }
