f88c09b56d6c4479d524279e233322d128b6ac81
Praveshkacode <pravesh182003@gmail.com>
2025-04-22 21:34:17 +0530

    add top expert in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
+import TopExperts from '../components/TopExperts'
 
 const Home = () => {
   return (
     <div>
       <Header/>
       <SpecialityMenu/>
+      <TopExperts/>
     </div>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// // tailwind.config.js
+// export default {
+//     content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
+//     theme: {
+//       extend: {
+//         gridTemplateColumns: {
+//           auto: 'repeat(auto-fill, minmax(200px, 1fr))',
+//         },
+//         // colors: {
+//         //   primary: '#5f6FFF',
+//         // },
+//       },
+//     },
+//     plugins: [],
+//   }
+  
