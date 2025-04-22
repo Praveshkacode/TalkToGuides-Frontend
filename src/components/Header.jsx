9312ce1b6714e36b7eeba5b9d057a94e45b28047
Praveshkacode <pravesh182003@gmail.com>
2025-04-22 09:44:01 +0530

    add speciality menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
             <img className='w-28' src={assets.group_profiles} alt="" />
             <p>Simply browse through our expert list of trusted Guide,<br className='hidden sm:block'/> schedule your session hassle-free.</p>
         </div>
-        <a href="" className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300'>Book session <img  className='w-3' src={assets.arrow_icon} alt="" /></a>
+        <a href="#-speciality" className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300'>Book session <img  className='w-3' src={assets.arrow_icon} alt="" /></a>
       </div>
       {/* right side */}
       <div className='md:w-1/2 relative'>
