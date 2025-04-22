951c2c388e736389a3ef3c24571b6c45a11ac566
Praveshkacode <pravesh182003@gmail.com>
2025-04-22 23:40:15 +0530

    add font

--- src/components/TopExperts.jsx
+++ src/components/TopExperts.jsx
@@ -14,7 +14,7 @@ const TopExperts = () => {
         Simply browse through our extensive list of trusted experts.
       </p>
       <div className='w-full grid gap-4 pt-5 gap-y-6 px-3 sm:px-0 ' style={{ gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))' }}>
-        {psychicExperts.slice(0,10).map((item,index)=>(
+        {psychicExperts.slice(0,8).map((item,index)=>(
           <div onClick={()=>navigate(`/session/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500 ' key={index}>
             <img className='bg-blue-50' src={item.image} alt="" />
             <div className='p-4'>
